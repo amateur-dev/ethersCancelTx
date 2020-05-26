d1d635d674083201f1ac520c4aa0c53b60f7cc40
amateur-dev <me@dipeshsukhani.dev>
2020-05-26 17:41:06 +0800

    tweaked didMount fx to sign message

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { ethers } from 'ethers';
 import "./App.css";
 
 class App extends Component {
-  state = { web3: null, accounts: null, provider: null };
+  state = { web3: null, accounts: null, provider: null, nonce: null, signer: null};
 
   componentDidMount = async () => {
     try {
@@ -20,7 +20,13 @@ class App extends Component {
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts });
       let provider = new ethers.providers.Web3Provider(web3.currentProvider);
-      this.setState({provider})
+      
+      var signer = provider.getSigner();
+      this.setState({provider, signer})
+      let nonce = await provider.getTransactionCount(accounts[0]);
+      this.setState({nonce})
+      let signature = await signer.signMessage("Hello world");
+      console.log(signature);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -38,6 +44,7 @@ class App extends Component {
       <div className="App">
         <h1>Good to Go!</h1>
         <div>You account is {this.state.accounts}</div>
+        <div>{this.state.nonce}</div>
       </div>
     );
   }
