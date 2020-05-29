7901ee0baa0a114f21aeb68bdeadbf2a30939e01
amateur-dev <me@dipeshsukhani.dev>
2020-05-29 13:58:29 +0800

    using aragon's wallet

--- src/App.js
+++ src/App.js
@@ -1,55 +1,36 @@
-import React, { Component } from "react";
-import getWeb3 from "./getWeb3";
-import { ethers } from 'ethers';
+import React from 'react'
+import { useWallet, UseWalletProvider } from 'use-wallet'
 
+function App() {
+  const wallet = useWallet()
+  const blockNumber = wallet.getBlockNumber()
 
-import "./App.css";
-
-class App extends Component {
-  state = { web3: null, accounts: null, provider: null, nonce: null, signer: null};
-
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts });
-      let provider = new ethers.providers.Web3Provider(web3.currentProvider);
-      
-      var signer = provider.getSigner();
-      var address = await signer.getAddress();
-      console.log(address);
-      this.setState({provider, signer})
-      let nonce = await provider.getTransactionCount(accounts[0]);
-      this.setState({nonce})
-      let signature = await signer.signMessage("Hello world");
-      console.log(signature);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts`
-      );
-      console.error(error);
-    }
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3 and accounts</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <div>You account is {this.state.accounts}</div>
-        <div>{this.state.nonce}</div>
-      </div>
-    );
-  }
+  return (
+    <>
+      <h1>Wallet</h1>
+      {wallet.connected ? (
+        <div>
+          <div>Account: {wallet.account}</div>
+          <div>Balance: {wallet.balance}</div>
+          <button onClick={() => wallet.deactivate()}>disconnect</button>
+        </div>
+      ) : (
+        <div>
+          Connect:
+          <button onClick={() => wallet.activate()}>MetaMask</button>
+        </div>
+      )}
+    </>
+  )
 }
 
-export default App;
+// Wrap everything in <UseWalletProvider />
+export default () => (
+  <UseWalletProvider
+    chainId={1}
+    connectors={{
+    }}
+  >
+    <App />
+  </UseWalletProvider>
+)
